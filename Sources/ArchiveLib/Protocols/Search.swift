2c77fc099d654b57b9c36fdafa9495b08ee5df76
Julian Kahnert <mail@juliankahnert.de>
2018-11-14 20:04:08 +0100

    add search tests

--- Sources/ArchiveLib/Protocols/Search.swift
+++ Sources/ArchiveLib/Protocols/Search.swift
@@ -7,25 +7,39 @@
 
 import Foundation
 
+/// Protocol for objects which should be searched.
 protocol Searchable: Hashable {
+
+    /// Term which will be used for the search
     var searchTerm: String { get }
 }
 
+/// Protocol for objects which can search.
 protocol Searcher {
     associatedtype Element: Searchable
 
+    /// Set of all the searchable elements.
     var allSearchElements: Set<Element> { get }
 
     func filterBy(_ searchTerm: String) -> Set<Element>
     func filterBy(_ searchTerms: [String]) -> Set<Element>
 }
 
+// MARK: - Implementation of the Searcher functions.
 extension Searcher {
 
+    /// Filter the "Searchable" objects by a search term.
+    ///
+    /// - Parameter searchTerm: Searchable object must contain the specified search term.
+    /// - Returns: All objects which stickt to the constraints.
     func filterBy(_ searchTerm: String) -> Set<Element> {
         return filterBy(searchTerm, allSearchElements)
     }
 
+    /// Filter the "Searchable" objects by all search terms.
+    ///
+    /// - Parameter searchTerms: Searchable object must contain all the specified search terms.
+    /// - Returns: All objects which stickt to the constraints.
     func filterBy(_ searchTerms: [String]) -> Set<Element> {
         // all searchTerms must be machted
 
@@ -36,6 +50,12 @@ extension Searcher {
         return currentElements
     }
 
+    /// Internal filter function.
+    ///
+    /// - Parameters:
+    ///   - searchTerm: Searchable object must contain the specified search term.
+    ///   - searchElements: Objects which should be searched.
+    /// - Returns: All objects which stickt to the constraints.
     private func filterBy(_ searchTerm: String, _ searchElements: Set<Element>) -> Set<Element> {
         return searchElements.filter { $0.searchTerm.contains(searchTerm) }
     }
---
+++ Tests/ArchiveLibTests/SearchTests.swift
@@ -0,0 +1,105 @@
+//
+//  SearchTests.swift
+//  ArchiveLibTests
+//
+//  Created by Julian Kahnert on 14.11.18.
+//
+
+@testable import ArchiveLib
+import XCTest
+
+class TestElement: Searchable {
+    var searchTerm: String
+    let hashValue: Int
+
+    init(filename: String) {
+        self.hashValue = filename.hashValue
+        self.searchTerm = filename
+    }
+
+    static func == (lhs: TestElement, rhs: TestElement) -> Bool {
+        return lhs.searchTerm == rhs.searchTerm
+    }
+
+}
+
+class TestSearcher: Searcher {
+    typealias Element = TestElement
+
+    var allSearchElements: Set<TestElement> = []
+}
+
+class SearchTests: XCTestCase {
+
+    func testSearch1() {
+        
+        // prepare
+        let element1 = TestElement(filename: "2018 05 12 kitchen table bill ikea")
+        let element2 = TestElement(filename: "2018 01 07 tom tailor shirt bill")
+        
+        let index = TestSearcher()
+        index.allSearchElements.insert(element1)
+        index.allSearchElements.insert(element2)
+        
+        // act
+        let foundElements = index.filterBy("bill")
+        
+        
+        // assert
+        XCTAssertTrue(foundElements.contains(element1))
+        XCTAssertTrue(foundElements.contains(element2))
+    }
+    
+    func testSearch2() {
+        
+        // prepare
+        let element1 = TestElement(filename: "2018 05 12 kitchen table bill ikea")
+        let element2 = TestElement(filename: "2018 01 07 tom tailor shirt bill")
+        
+        let index = TestSearcher()
+        index.allSearchElements.insert(element1)
+        index.allSearchElements.insert(element2)
+        
+        // act
+        let foundElements = index.filterBy(["shirt", "bill"])
+        
+        
+        // assert
+        XCTAssertFalse(foundElements.contains(element1))
+        XCTAssertTrue(foundElements.contains(element2))
+    }
+
+    func testFilterPerformance1() {
+
+        // create the search base
+        let index = TestSearcher()
+        for idx in stride(from: 0, to: 100, by: 1) {
+            index.allSearchElements.insert(TestElement(filename: "2018 05 12 document\(idx) description tag\(idx) tag\(idx * 11)"))
+        }
+
+        // performance test with a lot results
+        var filteredElements = Set<TestElement>()
+        self.measure {
+            // Put the code you want to measure the time of here.
+            filteredElements = index.filterBy("description")
+        }
+        XCTAssertEqual(filteredElements.capacity, 192)
+    }
+
+    func testFilterPerformance2() {
+
+        // create the search base
+        let index = TestSearcher()
+        for idx in stride(from: 0, to: 100, by: 1) {
+            index.allSearchElements.insert(TestElement(filename: "2018 05 12 document\(idx) description tag\(idx) tag\(idx * 11)"))
+        }
+
+        // performance test with only a few results
+        var filteredElements = Set<TestElement>()
+        self.measure {
+            // Put the code you want to measure the time of here.
+            filteredElements = index.filterBy("tag11")
+        }
+        XCTAssertEqual(filteredElements.capacity, 3)
+    }
+}
