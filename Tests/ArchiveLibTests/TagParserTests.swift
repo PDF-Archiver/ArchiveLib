56deabbd18c280277e3d578394ac8766498672a0
Julian Kahnert <mail@juliankahnert.de>
2019-01-13 13:36:34 +0100

    fix in unit test

--- Tests/ArchiveLibTests/TagParserTests.swift
+++ Tests/ArchiveLibTests/TagParserTests.swift
@@ -13,15 +13,7 @@ class TagParserTests: XCTestCase {
     func testParsingValidTags() {
 
         // setup the raw string
-        let longText = """
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-        At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, 20050201 no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        """
         let rawStringMapping: [String: Set<String>] = [
-            longText: Set(["clita", "gubergren", "kasd", "stet"]),
             "This is a IKEA tradfri bulb!": Set(["ikea"]),
             "Bill of an Apple MacBook.": Set(["apple", "bill", "macbook"])
         ]
